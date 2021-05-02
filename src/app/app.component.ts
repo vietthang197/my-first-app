0e68421ffe2b27054d6fe89b0bd87c160d0ff5d0
Lê Việt Thắng <levietthang1997@gmail.com>
2021-05-02 15:57:24 +0700

    note

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name: string = '';
+  listName: Array<string> = [];
+
+  updateListName = () => {
+    this.listName.push(this.name);
+    this.name = '';
+  }
+
+  clearListName = () => {
+    this.listName = [];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { Server2Component } from './server2/server2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    Server2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
