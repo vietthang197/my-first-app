df97e4267015f2463ed7b3a183d779b9b44acb64
Lê Việt Thắng <levietthang1997@gmail.com>
2021-05-05 22:47:27 +0700

    update code

--- src/app/cokpit/cokpit.component.ts
+++ src/app/cokpit/cokpit.component.ts
@@ -1,15 +1,26 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
 import {Server} from '../shared/server.model';
 
 @Component({
   selector: 'app-cokpit',
   templateUrl: './cokpit.component.html',
-  styleUrls: ['./cokpit.component.css']
+  styleUrls: ['./cokpit.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class CokpitComponent implements OnInit {
+export class CokpitComponent implements OnInit, AfterViewInit {
 
   @Output() serverCreated = new EventEmitter<Server>();
   @Output() blueprintCreated = new EventEmitter<Server>();
+  @ViewChild('serverNameInput', {static: false}) serverNameInput;
 
   serverName: string = '';
   serverContent: '';
@@ -18,13 +29,19 @@ export class CokpitComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.serverNameInput)
   }
 
   onServerAdd() {
+    console.log(this.serverNameInput)
     this.serverCreated.emit(new Server(this.serverName, 'server', this.serverContent));
   }
 
   onServerBlueprintAdd() {
     this.blueprintCreated.emit(new Server(this.serverName, 'blueprint', this.serverContent));
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.serverNameInput)
   }
 }
