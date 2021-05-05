8440ffaf2c95b9baeca1ccc29062ba8ba4976d87
Lê Việt Thắng <levietthang1997@gmail.com>
2021-05-05 12:33:52 +0700

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { Server2Component } from './server2/server2.component';
+import { CokpitComponent } from './cokpit/cokpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    Server2Component
+    Server2Component,
+    CokpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
